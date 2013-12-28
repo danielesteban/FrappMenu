0ceb1481a995c447a6b9607273ac8dfab3fe67d2
Daniel Esteban Nombela <dani@hellacoders.com>
2013-12-27 22:09:38 -0600

    Initial commit

---
+++ js/frapp.js
@@ -0,0 +1,64 @@
+FRAPPSMENU = {
+	create : function() {
+		/* Show modal */
+		SESSION.signin(function() {
+			var modal = $(Handlebars.partials.create());
+			modal.on('hidden.bs.modal', function() {
+				$(this).remove();
+			});
+			modal.on('shown.bs.modal', function() {
+				$('input', this).first().focus();
+			});
+			$('form', modal).submit(function(e) {
+				e.stopPropagation();
+				e.preventDefault();
+				var name = e.target.name.value;
+				FRAPP.create({
+					name : name
+				}, function(frapp) {
+					if(!frapp) return;
+					FRAPP.load(frapp, {}, true);
+					FRAPP.edit(frapp);
+				});
+			});
+			$('body').append(modal);
+			modal.modal('show');
+		});
+	}
+};
+
+window.addEventListener('frapp.init', function() {
+	/* Handlebars helpers */
+	Handlebars.registerHelper('i', function(className) {
+		return new Handlebars.SafeString('<span class="glyphicon glyphicon-' + className + '"></span>');
+	});
+
+	/* Get & render installed Frapps */
+	FRAPP.installed(function(frapps) {
+		var engineFrapps = [
+				'https://github.com/danielesteban/FrappsMenu.git',
+				'https://github.com/danielesteban/FrappSignin.git'
+			],
+			l = frapps.length;
+
+		for(var i=0; i<l; i++) {
+			if(engineFrapps.indexOf(frapps[i].repository.url) === -1) continue;
+			frapps.splice(i, 1);
+			l--;
+			i--;
+		}
+		$('body').append(Handlebars.templates.frapps({
+			version : FRAPP.version.engine,
+			year : (new Date()).getFullYear(),
+			frapps : frapps
+		})).css('overflow', 'auto');
+		$('menu.frapps a').click(function(e) {
+			var li = $(e.target).parents('li').first();
+			if(li.attr('class') === 'add') {
+				//TODO!!
+			} else {
+				FRAPP.load(frapps[li.index()], {}, true);
+			}
+		});
+	});
+});
---
+++ js/lang.js
@@ -0,0 +1,22 @@
+LANG = {
+	en : {
+		createYourOwn : 'Create your own',
+		all : 'All',
+		yourFrapps : 'Your Frapps',
+		addMore : 'Add More',
+		underGPL : 'Under a GPL license',
+		createNew : 'Create New Frapp',
+		name : 'Name',
+		create : 'Create'
+	},
+	es : {
+		createYourOwn : 'Crear una tuya',
+		all : 'Todas',
+		yourFrapps : 'Tus Frapps',
+		addMore : 'Añadir Más',
+		underGPL : 'Bajo una licencia GPL',
+		createNew : 'Crear Nueva Frapp',
+		name : 'Nombre',
+		create : 'Crear'
+	}
+};
